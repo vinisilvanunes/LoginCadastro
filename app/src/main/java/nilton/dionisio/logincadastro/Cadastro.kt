5f4319cd86e037c49e0bcd170bca5911bb592625
labs <niltondg.39@gmail.com>
2024-06-15 11:22:38 -0300

    Layout do Cadastro

---
+++ app/src/main/java/nilton/dionisio/logincadastro/Cadastro.kt
@@ -0,0 +1,11 @@
+package nilton.dionisio.logincadastro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Cadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+    }
+}
